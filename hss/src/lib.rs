ea6686f129802d8f715374894cbd16ab21c0393f
Tsumida <elsa110249@163.com>
2021-03-02 22:33:10 +0800

    add mysql test

--- hss/src/lib.rs
+++ hss/src/lib.rs
@@ -2,17 +2,20 @@
 
 // pub mod sqlite;
 
+use cryptokit::{DefaultSignaturer, Signaturer};
 use hotstuff_rs::safety::machine::{SafetyStorage, Snapshot};
 use hs_data::*;
 use hs_data::{TreeNode, ViewNumber};
-use log::{debug, info};
+use log::{debug, error, info};
 use pacemaker::{
-    data::{BranchData, BranchSyncStrategy, TimeoutCertificate},
+    data::{combine_time_certificates, BranchData, BranchSyncStrategy, TimeoutCertificate},
     liveness_storage::{LivenessStorage, LivenessStorageErr},
 };
+use sqlx::{mysql::MySqlRow, Row};
 use std::{
-    collections::{HashMap, VecDeque},
-    sync::Arc,
+    collections::{BTreeMap, HashMap, HashSet, VecDeque},
+    future::Future,
+    sync::{Arc, Condvar},
 };
 
 pub struct InMemoryState {
@@ -39,10 +42,18 @@ pub struct InMemoryState {
 
 /// HotstuffStorage cache changes in memory and flush dirty data.
 pub struct HotstuffStorage {
-    backend: PersistentStorage,
+    backend: MysqlStorage,
     state: InMemoryState,
 
-    tc_queue: VecDeque<TimeoutCertificate>,
+    conf: HotStuffConfig,
+
+    signaturer: DefaultSignaturer,
+
+    tc_tree: BTreeMap<ViewNumber, HashSet<TimeoutCertificate>>,
+
+    combined_tc_queue: VecDeque<TimeoutCertificate>,
+    partial_tc_queue: VecDeque<TimeoutCertificate>,
+
     /// consider case: `3<-4<-4`
     /// proposal `3<-4` should be appended into prop_queue and
     /// the second 4's qc should be appended into justify_queue.
@@ -50,20 +61,20 @@ pub struct HotstuffStorage {
     justify_queue: VecDeque<GenericQC>,
 }
 
-/// Persistent storage based on mysql. PersistentStorage promises that:
+/// Persistent storage based on mysql. MysqlStorage promises that:
 /// - It won't lose any proposal with a QuorumCertificate, once received.
 /// - It won't lose any QuorumCertificate and TimeCertificate, once received.
 ///
 /// For example , Persistent Stroage keeps `3<-4` in branch `3<-4<-4` and never lose them.
 /// QC carried by the second `4` should be stablized but no guarantees for the second proposal `4` itself.
 ///
-pub struct PersistentStorage {
+pub struct MysqlStorage {
     // A pool for proposals that doesn't form qc.
     in_mem_queue: HashMap<NodeHash, Arc<TreeNode>>,
     conn_pool: sqlx::MySqlPool,
 }
 
-impl PersistentStorage {
+impl MysqlStorage {
     fn get(&self, node_hash: &NodeHash) -> Option<Arc<TreeNode>> {
         // search cache.
         match self.in_mem_queue.get(node_hash) {
@@ -75,12 +86,35 @@ impl PersistentStorage {
 
     fn insert(&mut self, node_hash: NodeHash, prop: Arc<TreeNode>) {}
 
-    fn flush(&mut self) {}
+    async fn async_flush(&mut self) -> Result<(), sqlx::Error> {
+        // start rx
+        let mut tx = self.conn_pool.begin().await?;
+
+        // flush queues;
+        // flush state
+        tx.commit().await
+    }
+}
+
+pub struct HotStuffConfig {
+    total: usize,
+    peers_addr: HashMap<ReplicaID, String>,
+}
+
+impl HotStuffConfig {
+    pub fn threshold(&self) -> usize {
+        (self.total << 1) / 3
+    }
 }
 
 impl HotstuffStorage {
-    // TODO: remove qcs
-    pub fn new(view: ViewNumber, init_node: &TreeNode, init_qc: &GenericQC) -> Self
+    pub fn new(
+        view: ViewNumber,
+        init_node: &TreeNode,
+        init_qc: &GenericQC,
+        conf: HotStuffConfig,
+        signaturer: DefaultSignaturer,
+    ) -> Self
     where
         Self: Sized,
     {
@@ -97,30 +131,128 @@ impl HotstuffStorage {
                 // height\view of the leaf.
                 qc_high: Arc::new(init_qc.clone()),
             },
-            tc_queue: VecDeque::with_capacity(8),
+            signaturer,
+            conf,
+            combined_tc_queue: VecDeque::with_capacity(8),
+            partial_tc_queue: VecDeque::with_capacity(8),
             prop_queue: VecDeque::with_capacity(8),
             justify_queue: VecDeque::with_capacity(8),
+            tc_tree: BTreeMap::new(),
         }
     }
 
-    pub fn flush(&mut self) {}
+    pub async fn async_recover(&mut self) {
+        // refactor
+        // load all proposals;
+        let stream = sqlx::query(
+            "
+            select * from proposal inner join qc 
+            on proposal.justify_view=qc.view 
+            order by proposal.justify_view;
+            ",
+        )
+        .map(|row: MySqlRow| {
+            //  0       1               2           3          4       5        6               7
+            // view | parent_hash | justify_view | prop_hash | tx | qc.view | qc.node_hash | qc.combined_sign
+            let view: ViewNumber = row.get(0);
+            let parent_hash: String = row.get(1);
+            let justify_view: ViewNumber = row.get(2);
+            let prop_hash: String = row.get(3);
+            let tx: String = row.get(4);
+            let node_hash: String = row.get(6);
+            let combined_sign: String = row.get(7);
+
+            let node = TreeNode {
+                height: view,
+                txs: serde_json::from_str(&tx).unwrap(),
+                parent: NodeHash::from_vec(&base64::decode(&parent_hash).unwrap()),
+                justify: GenericQC::new(
+                    view,
+                    &NodeHash::from_vec(&base64::decode(&prop_hash).unwrap()),
+                    &combined_sign_from_vec_u8(base64::decode(&combined_sign).unwrap()),
+                ),
+            };
+        })
+        .fetch_all(&self.backend.conn_pool)
+        .await
+        .unwrap();
+
+        // load hotstuff_state
+
+        // load all tc_map;
+    }
+
+    pub async fn async_flush(&mut self) {
+        match self.backend.async_flush().await {
+            Ok(()) => {
+                debug!("flushed");
+            }
+            Err(e) => {
+                error!("{:?}", e);
+            }
+        }
+    }
+
+    pub fn block_flush(&mut self) {
+        // refactor
+        tokio::runtime::Runtime::new()
+            .unwrap()
+            .block_on(self.async_flush());
+    }
+
+    pub fn threshold(&self) -> usize {
+        (self.conf.total << 1) / 3
+    }
+
+    fn compress_tc(&mut self) -> Option<ViewNumber> {
+        // compress
+        let view = self
+            .tc_tree
+            .iter()
+            .filter(|(k, s)| s.len() >= self.conf.threshold())
+            .max_by_key(|(k, _)| *k)
+            .map(|(view, _)| *view);
+
+        if let Some(v) = view {
+            let from = String::default();
+            self.tc_tree
+                .remove(&v)
+                .and_then(|tc| {
+                    combine_time_certificates(
+                        &self.signaturer,
+                        &from,
+                        v,
+                        self.get_qc_high().as_ref().clone(),
+                        &tc,
+                    )
+                })
+                .map_or((), |combined_tc| {
+                    self.combined_tc_queue.push_back(combined_tc);
+                });
+        }
+        view
+    }
 }
 
 impl SafetyStorage for HotstuffStorage {
     fn flush(&mut self) -> hotstuff_rs::safety::machine::Result<()> {
-        todo!()
+        self.block_flush();
+        Ok(())
     }
 
     fn append_new_node(&mut self, node: &TreeNode) {
         let h = TreeNode::hash(node);
-        self.backend.insert(h, Arc::new(node.clone()));
+        let node = Arc::new(node.clone());
+
+        // Warning: OOM since every node has a replica in memory.
+        self.backend.insert(h, node.clone());
+        self.prop_queue.push_back(node);
     }
 
     fn append_new_qc(&mut self, qc: &GenericQC) {
         self.justify_queue.push_back(qc.clone());
     }
 
-    // todo: refactor
     fn find_three_chain(&self, node: &TreeNode) -> Vec<Arc<TreeNode>> {
         let mut chain = Vec::with_capacity(3);
         if let Some(b3) = self.backend.get(node.justify().node_hash()) {
@@ -182,7 +314,7 @@ impl SafetyStorage for HotstuffStorage {
                 break;
             }
         }
-        parent_hash != TreeNode::hash(b)
+        height_now == b.height() && parent_hash != TreeNode::hash(b)
     }
 
     fn get_node(&self, node_hash: &NodeHash) -> Option<Arc<TreeNode>> {
@@ -251,7 +383,6 @@ impl SafetyStorage for HotstuffStorage {
     fn update_vheight(&mut self, vheight: ViewNumber) -> ViewNumber {
         let prev = self.state.vheight;
         self.state.vheight = ViewNumber::max(self.state.vheight, vheight);
-
         prev
     }
 
@@ -288,33 +419,65 @@ impl SafetyStorage for HotstuffStorage {
 
 impl LivenessStorage for HotstuffStorage {
     fn append_tc(&mut self, tc: TimeoutCertificate) -> Result<ViewNumber, LivenessStorageErr> {
-        todo!()
+        match tc.view_sign() {
+            pacemaker::data::SignType::Partial(_) => {
+                self.partial_tc_queue.push_back(tc);
+            }
+            pacemaker::data::SignType::Combined(_) => {
+                self.combined_tc_queue.push_back(tc);
+            }
+        }
+        // current_view - 1 is ok
+        Ok(self.compress_tc().unwrap_or(self.get_view() - 1))
     }
 
     fn is_reach_threshold(&self, view: ViewNumber) -> bool {
-        todo!()
+        self.tc_tree
+            .get(&view)
+            .map_or(false, |s| s.len() >= self.conf.threshold())
     }
 
-    fn update_qc_high(&mut self, qc: &GenericQC) -> Result<(), LivenessStorageErr> {
-        todo!()
+    fn update_qc_high(
+        &mut self,
+        node: &TreeNode,
+        qc: &GenericQC,
+    ) -> Result<(), LivenessStorageErr> {
+        <HotstuffStorage as SafetyStorage>::update_qc_high(self, node, qc);
+        Ok(())
     }
 
     fn fetch_branch(
         &self,
         strategy: &BranchSyncStrategy,
     ) -> Result<BranchData, LivenessStorageErr> {
-        todo!()
+        // refactor
+        match strategy {
+            BranchSyncStrategy::Grow {
+                grow_from,
+                batch_size,
+            } => {
+                let v: Vec<TreeNode> = self
+                    .backend
+                    .in_mem_queue
+                    .values()
+                    .filter(|s| &s.height() > grow_from)
+                    .map(|node| node.as_ref().clone())
+                    .collect();
+
+                Ok(BranchData { data: v })
+            }
+        }
     }
 
     fn is_qc_node_exists(&mut self, qc: &GenericQC) -> bool {
-        todo!()
+        self.backend.get(&qc.node_hash()).is_some()
     }
 
     fn get_locked_node(&mut self) -> &TreeNode {
-        todo!()
+        self.state.b_locked.as_ref()
     }
 
     fn get_leaf(&self) -> &TreeNode {
-        todo!()
+        self.state.leaf.as_ref()
     }
 }
--- hss/tests/mod.rs
+++ hss/tests/mod.rs
@@ -1,1 +1,198 @@
 //! test for HotstuffStorage
+
+use futures::StreamExt;
+use hs_data::{combined_sign_from_vec_u8, CombinedSign, NodeHash};
+use log::error;
+use sqlx::{mysql::MySqlRow, Executor, Row};
+use std::mem::MaybeUninit;
+
+fn init_logger() {
+    use simplelog::*;
+    let _ = CombinedLogger::init(vec![TermLogger::new(
+        LevelFilter::Info,
+        Config::default(),
+        TerminalMode::Mixed,
+    )]);
+}
+
+struct MockerDB {
+    conn: sqlx::mysql::MySqlPool,
+}
+
+impl MockerDB {
+    async fn new() -> Self {
+        let conn = sqlx::mysql::MySqlPool::connect(
+            "mysql://root:helloworld@localhost:3306/hotstuff_test_mocker",
+        )
+        .await
+        .unwrap();
+
+        conn.execute(
+            "
+        CREATE TABLE IF NOT EXISTS `combined_tc`
+        (
+        `view`          bigint unsigned NOT NULL ,
+        `combined_sign` varchar(128) NOT NULL ,
+
+        PRIMARY KEY (`view`)
+        );
+
+
+        CREATE TABLE IF NOT EXISTS `hotstuff_state`
+        (
+        `token`           varchar(64) NOT NULL ,
+        `current_view`    bigint unsigned NOT NULL ,
+        `last_voted_view` bigint unsigned NOT NULL ,
+        `locked_view`     bigint unsigned NOT NULL ,
+        `committed_view`  bigint unsigned NOT NULL ,
+        `executed_view`   bigint unsigned NOT NULL ,
+
+        PRIMARY KEY (`token`)
+        );
+
+
+        CREATE TABLE IF NOT EXISTS `partial_tc`
+        (
+        `view`         bigint unsigned NOT NULL ,
+        `partial_sign` varchar(128) NOT NULL ,
+        `replica_id`   varchar(64) NOT NULL ,
+
+        PRIMARY KEY (`view`, `replica_id`)
+        );
+
+
+        CREATE TABLE IF NOT EXISTS `peers`
+        (
+        `replica_id` varchar(64) NOT NULL ,
+        `addr`       varchar(64) NOT NULL ,
+
+        PRIMARY KEY (`replica_id`)
+        );
+
+
+        CREATE TABLE IF NOT EXISTS `proposal`
+        (
+        `view`         bigint unsigned NOT NULL ,
+        `parent_hash`  varchar(128) NOT NULL ,
+        `justify_view` bigint unsigned NOT NULL ,
+        `prop_hash`    varchar(128) NOT NULL ,
+        `txn`          mediumblob NOT NULL ,
+
+        PRIMARY KEY (`view`)
+        );
+
+
+        CREATE TABLE IF NOT EXISTS `qc`
+        (
+        `view`          bigint unsigned NOT NULL ,
+        `node_hash`     varchar(128) NOT NULL ,
+        `combined_sign` varchar(128) NOT NULL ,
+
+        PRIMARY KEY (`view`)
+        );",
+        )
+        .await
+        .unwrap();
+
+        MockerDB { conn }
+    }
+
+    async fn close(&mut self) {
+        self.conn
+            .execute(
+                "
+            DROP TABLE IF EXISTS `combined_tc`;
+            DROP TABLE IF EXISTS `proposal`;
+            DROP TABLE IF EXISTS `qc`;
+            DROP TABLE IF EXISTS `partial_tc`;
+            DROP TABLE IF EXISTS `hotstuff_state`;
+            DROP TABLE IF EXISTS `peers`;
+            ",
+            )
+            .await
+            .unwrap();
+    }
+}
+
+/// Test and usage of sqlx.
+async fn test_1() -> Result<(), sqlx::Error> {
+    let mut db = MockerDB::new().await;
+
+    let init_prop = hs_data::INIT_NODE.clone();
+    let parent_hash: String =
+        base64::encode::<NodeHash>(unsafe { MaybeUninit::uninit().assume_init() });
+    let node_hash = base64::encode(hs_data::INIT_NODE_HASH.as_ref());
+    let combined_sign: CombinedSign = unsafe { MaybeUninit::uninit().assume_init() };
+    let sign = base64::encode(&combined_sign.to_bytes());
+
+    // stablization
+
+    let mut tx = db.conn.begin().await.unwrap();
+    sqlx::query(
+        "
+        insert into proposal
+        (view, parent_hash, justify_view, prop_hash, txn)
+        values
+        (?, ?, ?, ?, ?)
+    ;",
+    )
+    .bind(&init_prop.height())
+    .bind(&parent_hash)
+    .bind(0u64)
+    .bind(&node_hash)
+    .bind(&serde_json::to_string::<Vec<u8>>(&vec![]).unwrap())
+    .execute(&mut tx)
+    .await
+    .unwrap();
+
+    sqlx::query(
+        "
+        insert into qc
+        (view, node_hash, combined_sign)
+        values
+        (?, ?, ?)
+    ;",
+    )
+    .bind(0u64)
+    .bind(&node_hash)
+    .bind(&sign)
+    .execute(&mut tx)
+    .await
+    .unwrap();
+    tx.commit().await.unwrap();
+
+    //  0       1               2           3          4       5        6               7
+    // view | parent_hash | justify_view | prop_hash | tx | qc.view | qc.node_hash | qc.combined_sign
+    let (v, p, j, h, s) =
+        sqlx::query("select * from proposal inner join qc on proposal.view=qc.view limit 1;")
+            .map(|row: MySqlRow| {
+                let s: (u64, String, u64, String, String) =
+                    (row.get(0), row.get(1), row.get(2), row.get(3), row.get(7));
+                s
+            })
+            .fetch_one(&db.conn)
+            .await
+            .unwrap();
+
+    assert!(v == 0 && p == parent_hash && j == 0 && h == node_hash && s == sign);
+
+    let transferred = combined_sign_from_vec_u8(base64::decode(&s).unwrap());
+    assert!(
+        combined_sign == transferred,
+        "\n{:?}\n{:?}",
+        combined_sign,
+        transferred
+    );
+
+    db.close().await;
+    Ok(())
+}
+
+#[test]
+fn test_sqlx() {
+    init_logger();
+    tokio::runtime::Runtime::new()
+        .unwrap()
+        .block_on(test_1())
+        .unwrap();
+}
